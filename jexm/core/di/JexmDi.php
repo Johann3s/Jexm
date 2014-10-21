7c176ff8cde50ca348e3c36863c1f321da45e7d9
Johann3s <YHJohVir001@student.jenseneducation.se>
2014-10-21 23:12:04 +0200

    Adding a dependancy injector

--- jexm/core/di/JexmDi.php
+++ jexm/core/di/JexmDi.php
@@ -1,23 +1,75 @@
 <?php
 	namespace jexm\core\di;
+	use \jexm\core\BaseHelper as BaseHelper;
 	
 	class JexmDi{
 		
+		protected $objects = [];
+		protected $instantiatedObjects = [];
+		protected $reflection;
 		
-		public static function get($class){
-			$dependencies = self::getDependencies($class);
+		
+		/**
+		* Point of access from outside. Inspects class and returns class instantly if no constructor exist. 
+		* Otherwise piles down its dependencies and returns objects.
+		* @param string Namespaced class
+		* @return object Instantiated class with dependencies injected.(recursively)
+		*/
+		public function get($class){
+			
+			$this->reflection = new \ReflectionClass($class);
+			$constructor = $this->reflection->getConstructor();
+
+			if(is_null($constructor)){
+				return new $class();
+			}else{
+				$this->getDependencies($class,$constructor);
+				return $this->instantiateAndReturn($class);
+			}
 		}
 		
-		private static function getDependencies($class){
-			$reflection = new \ReflectionClass($class);
-			$constructor = $reflection->getConstructor();
-			//var_export($constructor);die();
+		
+		
+		
+		/**
+		* Fetches the params from constructor
+		*/
+		private function getDependencies($class,$constructor){
 			$params = $constructor->getParameters();
+			$this->gatherDependenciesFromParams($params);
+		}
+		
+		
+		
+		
+		/**
+		* Gathers the data representing class (classname with namespace)
+		* and populates objects array.
+		*/
+		private function gatherDependenciesFromParams($params){
 			foreach($params as $param){
-				$c = $param->getClass();
-				var_dump($c);
+				if($param->isArray()){
+					BaseHelper::kill("An array was injected into the constructor. Only instantiable objects may be injected. Please use a setter if an array is a dependency");
+				}
+				if(is_null($param->getClass())){
+					continue;
+				}
+				$this->objects[] = $param->getClass();
 			}
-			//var_export($params);
-			die();
+		}
+		
+		
+		
+		/**
+		* Instantiates class and injects dependencies recursively. 
+		* If dependency have dependencies they will be instantiated aswell.
+		*/
+		private function instantiateAndReturn($class){
+			
+			foreach($this->objects as $object){
+				$this->instantiatedObjects[] = (new JexmDi)->get($object->name);
+			}
+			return $this->reflection->newInstanceArgs($this->instantiatedObjects);
+			
 		}
 	}
