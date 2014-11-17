55aeae14a845efd7744bab96f4987fc92563b149
Johann3s <YHJohVir001@student.jenseneducation.se>
2014-11-18 00:53:29 +0100

    Readding usermodel

---
+++ jexm/models/User.php
@@ -0,0 +1,33 @@
+<?php
+	namespace jexm\models;
+	
+	class User extends Model{
+		
+		/**
+		* Change if tablename is NOT users
+		*/
+		protected $table = "users";
+		
+		
+		
+		public function __construct(){
+			parent::__construct();
+			if(!$tableExist = $this->tableExist($this->table)){
+				throw new \Exception("Unable to find table ".$this->table);
+			}
+		}
+		
+		
+		
+		/**
+		* Gets user credentials for authentication
+		* Values injected into sqlstring are NOT coming from user.
+		* @param array Associative array for fetching userdata given column. [colname => value]
+		* @return credentials or false
+		*/
+		public function getCredentials(array $accessors){
+			$accessorColName = array_keys($accessors);
+			$accessorValue = array_values($accessors);
+			return $this->fetch("SELECT * FROM {$this->table} WHERE {$accessorColName[0]} = ? LIMIT 1",[$accessorValue[0]]);
+		}
+	}
